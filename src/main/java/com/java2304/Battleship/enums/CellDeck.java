f749ccda0634bcc9e9869f2d8d31ec146c25333a
Dima Fedin <fedinamid@gmail.com>
2022-04-29 20:23:03 +0300

    Added first and second enums

---
+++ src/main/java/com/java2304/Battleship/enums/CellDeck.java
@@ -0,0 +1,6 @@
+package com.java2304.Battleship.enums;
+
+public enum CellDeck {
+    FIRST,
+    SECOND
+}
---
+++ src/main/java/com/java2304/Battleship/enums/MoveResult.java
@@ -0,0 +1,6 @@
+package com.java2304.Battleship.enums;
+
+public enum MoveResult {
+    HIT,
+    MISS
+}
